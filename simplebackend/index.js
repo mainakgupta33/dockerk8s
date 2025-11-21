cb28c329cebdbbffa1c245231f4c7cb958409cc5
mainakgupta33 <mainakgupta33@gmail.com>
2025-11-22 03:02:11 +0530

    added CORS package

--- simplebackend/index.js
+++ simplebackend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const dataFile = '/data/users.json';
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
